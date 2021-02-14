5586b4d9c8acb740c214a3fbcb156f6bf9f6eac1
DESKTOP-BNS43KJ\admin <xoceco@139.com>
2021-02-14 13:09:55 +0800

    2

--- app/src/main/java/com/dnake/myapplication/MainActivity.java
+++ app/src/main/java/com/dnake/myapplication/MainActivity.java
@@ -13,7 +13,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {//MainActivity
+public class MainActivity extends AppCompatActivity {//MainActivity22222
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
